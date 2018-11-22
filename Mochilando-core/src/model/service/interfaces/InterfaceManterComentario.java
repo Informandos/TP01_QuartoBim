d103ae4da059c974461d3b602a934a419c5ea328
epiphanio <eduardo.epiphanio.moreira@gmail.com>
2018-11-22 11:15:59 -0200

    comentado

--- Mochilando-core/src/model/service/interfaces/InterfaceManterComentario.java
+++ Mochilando-core/src/model/service/interfaces/InterfaceManterComentario.java
@@ -1,4 +1,5 @@
 /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -23,3 +24,4 @@ public interface InterfaceManterComentario {
     public List<Comentario> pesquisarPorCodDiario(Long codDiario) throws ExcecaoPersistencia,ExcecaoConexaoCliente;
     public List<Comentario> pesquisarTodos() throws ExcecaoPersistencia,ExcecaoConexaoCliente;
 }
+*/
