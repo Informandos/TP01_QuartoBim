58cc369fb759b5acc925995d177b5cf4353632a3
Juliana Carvalho <jujucarvalho.souza@yahoo.com.br>
2018-11-26 10:53:17 -0200

    Criando fábrica de conexões

---
+++ Mochilando-core/src/connection/ConnectionFactory.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Juliana
+ */
+public class ConnectionFactory {
+    
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("MochilaPU");
+    private static EntityManager em;
+
+    
+    public EntityManager getConnection(){
+        this.em = emf.createEntityManager();
+        return em;
+    }
+}
